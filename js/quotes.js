26b1084b764e9332068b533af74af25b8bf87c1f
dkellington <46822416+dkellington@users.noreply.github.com>
2019-01-23 11:07:19 -0500

    Quote Generator Project

---
+++ js/quotes.js
@@ -0,0 +1,21 @@
+var quotes = [
+{quote: 'You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.', 
+source: 'Dr. Seuss'},
+{quote: 'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.', 
+source: 'Mark Caine'},
+{quote: 'When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.', 
+source: 'Helen Keller'},
+{quote: 'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.', 
+source: 'Mark Twain'},
+{quote: 'Those who dare to fail miserably can achieve greatly.', source: 'John F. Kennedy'},
+{quote: 'You only live once, but if you do it right, once is enough.', source: 'Mae West'},
+{quote: 'He that falls in love with himself will have no rivals.', source: 'Benjamin Franklin'},
+{quote: 'It is hard to fail, but it is worse never to have tried to succeed.', source: 'Theodore Roosevelt'},
+{quote: 'Posterity! you will never know how much it cost the present generation to preserve your freedom! I hope you will make a good use of it. If you do not, I shall repent in Heaven that I ever took half the pains to preserve it.',
+source: 'John Adams', citation: 'A Letter to Abigail Adams', year: 1777},
+{quote: 'Everything that is really great and inspiring is created by the individual who can labor in freedom.', source: 'Dwight D. Eisenhower', 
+citation:'Speech during the commencement excercises of Swarthmore College', year: 1938},
+{quote: 'Give me liberty, or give me death!', source: 'Patrick Henry', 
+citation:'Second Virginia Convention', year: 1775}
+]
+
---
+++ js/script.js
@@ -0,0 +1,82 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 1 - A Random Quote Generator
+******************************************/
+
+/*
+The function 'getRandomQuote':
+  - receives an array
+  - calls the function 'getRandomNumber' to generate a random number i to use as an array index
+  - retrieves the array object located at index i
+  - returns array object to caller
+*/
+
+function getRandomQuote(array) {
+  var i = getRandomNumber(array.length);
+  var aQuote = array[i];
+  return aQuote;
+}
+
+/*
+The function 'getRandomNumber':
+  - receives the highest possible number of generated random number
+  - 1 is NOT added to generated random number because we want an array index, which starts with 0
+  - returns generated random number to caller
+*/
+
+function getRandomNumber(high) {
+  var randomNumber = Math.floor((Math.random() * high));
+  return randomNumber;
+}
+
+/*
+The function 'formatResult':
+  - receives a quote object
+  - determines what properties are contained by the object
+  - properly formats the object to print in the browser
+  - returns object formatted as a string to caller
+*/
+
+function formatResult(resultQuote) {
+  var result = '<p class="quote">' + resultQuote.quote + '</p>';
+  result += '<p class="source">' + resultQuote.source;
+  if (resultQuote.hasOwnProperty('citation')) {
+    result += '<span class="citation">' + resultQuote.citation + '</span>';
+    }
+  if (resultQuote.hasOwnProperty('year')) {
+    result += '<span class="year">' + resultQuote.year +  '</span>';
+    }
+  result += '</p>';
+  return result;
+}
+
+/*
+  The 'printQuote' function: 
+   - calls the 'getRandomQuote' function and assigns it to a variable, aQuote
+   - calls the 'formatResult' function to format the sent object into an HTML string
+   - sets the 'innerHTML' of the 'quote-box' div to the HTML string 
+*/
+
+function printQuote() {
+  var aQuote = getRandomQuote(quotes);
+  var stringOfQuoteProperties = formatResult(aQuote);
+  document.getElementById('quote-box').innerHTML = stringOfQuoteProperties;
+  }
+
+/***
+  When the "Show another quote" button is clicked, the event listener 
+  below will be triggered, and it will call, or "invoke", the `printQuote` 
+  function. So do not make any changes to the line of code below this 
+  comment.
+***/
+
+ document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+ //Display a quote when the page is created:
+
+ printQuote();
+
+
+
+
+
